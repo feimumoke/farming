79e15da1912afb016bde3cb3505669bf32f51393
hui.zhu <hui.zhu@shopee.com>
2021-12-16 22:15:53 +0800

    init pb

--- build/pb_gen.sh
+++ build/pb_gen.sh
@@ -5,7 +5,8 @@ BASE_PATH=$(
   pwd
 )
 CODE_PATH=${BASE_PATH}/..
-DOCKER_IMAGE='gate-way'
+DOCKER_IMAGE='feimumoke/go-gate-way:v1'
+#DOCKER_IMAGE='my-gt'
 
 # shellcheck disable=SC2046
 # take advantage of xargs construct arguments separated by space
@@ -20,9 +21,11 @@ while getopts "d:" arg; do
   esac
 done
 
-echo "Executing protocol buffer code and swagger generation commands: [5 in total]"
+echo "Executing protocol buffer commands"
 
 GOOGLE_PB_PKG_PATH="${CODE_PATH}/api"
+SERVER_PB_PATH="${GOOGLE_PB_PKG_PATH}/server/*.proto"
+SERVICE_PB_PATH="${GOOGLE_PB_PKG_PATH}/service/*.proto"
 # alias to docker cmd in docker-build mode
 if [ -z "${docker_build}" ]; then
   echo "[Using local tools]"
@@ -32,34 +35,25 @@ else
       echo "${DOCKER_IMAGE} not exist"
       docker pull ${DOCKER_IMAGE}
     fi
-    alias protoc='docker run -it -v ${CODE_PATH}:/data -w /data ${DOCKER_IMAGE} protoc'
+    alias protoc='docker run --rm -it -v ${CODE_PATH}:/data -w /data ${DOCKER_IMAGE} sh -c "protoc'
     alias find='docker run -it -v ${CODE_PATH}:/data -w /data ${DOCKER_IMAGE} find'
     # use latest googleapis in mod path or in api/googleapis
     GOOGLE_PB_PKG_PATH="/data/api"
+    SERVER_PB_PATH="${GOOGLE_PB_PKG_PATH}/server/*.proto\""
+    SERVICE_PB_PATH="${GOOGLE_PB_PKG_PATH}/service/*.proto\""
     echo "[Using tools in docker]"
   else
     echo "[Using local tools]"
   fi
 fi
 
-# shellcheck disable=SC2044
-for p in $(find "${GOOGLE_PB_PKG_PATH}/server" -name "*.proto"); do
-  echo $p
-  PB_GENERATE_CMD="protoc -I. -I api -I ${GOOGLE_PB_PKG_PATH}/googleapis --go_out=. --go-grpc_out=. --proto_path=${GOOGLE_PB_PKG_PATH}/server $p"
-  PB_PROXY_GENERATE_CMD="protoc -I. -I api -I ${GOOGLE_PB_PKG_PATH}/googleapis  --grpc-gateway_out=logtostderr=true:. --proto_path=${GOOGLE_PB_PKG_PATH}/server $p"
-  echo "$PB_GENERATE_CMD"
-  eval "${PB_GENERATE_CMD}"
-  echo "$PB_PROXY_GENERATE_CMD"
-  eval "${PB_PROXY_GENERATE_CMD}"
-done
+PB_GENERATE_CMD="protoc -I ${GOOGLE_PB_PKG_PATH}/googleapis --go_out=. --go-grpc_out=. --proto_path=${GOOGLE_PB_PKG_PATH}/server  ${SERVER_PB_PATH}"
+eval "${PB_GENERATE_CMD}"
+PB_PROXY_GENERATE_CMD="protoc -I ${GOOGLE_PB_PKG_PATH}/googleapis  --grpc-gateway_out=logtostderr=true:. --proto_path=${GOOGLE_PB_PKG_PATH}/server  ${SERVER_PB_PATH}"
+eval "${PB_PROXY_GENERATE_CMD}"
+PB_GENERATE_CMD="protoc -I ${GOOGLE_PB_PKG_PATH}/googleapis --go_out=. --go-grpc_out=. --proto_path=${GOOGLE_PB_PKG_PATH}/service  ${SERVICE_PB_PATH}"
+eval "${PB_GENERATE_CMD}"
+PB_PROXY_GENERATE_CMD="protoc -I ${GOOGLE_PB_PKG_PATH}/googleapis  --grpc-gateway_out=logtostderr=true:. --proto_path=${GOOGLE_PB_PKG_PATH}/service   ${SERVICE_PB_PATH}"
+eval "${PB_PROXY_GENERATE_CMD}"
 
-# shellcheck disable=SC2044
-for p in $(find "${GOOGLE_PB_PKG_PATH}/service" -name "*.proto"); do
-  echo $p
-  PB_GENERATE_CMD="protoc -I. -I api -I ${GOOGLE_PB_PKG_PATH}/googleapis --go_out=. --go-grpc_out=. --proto_path=${GOOGLE_PB_PKG_PATH}/service  $p"
-  PB_PROXY_GENERATE_CMD="protoc -I. -I api -I ${GOOGLE_PB_PKG_PATH}/googleapis  --grpc-gateway_out=logtostderr=true:. --proto_path=${GOOGLE_PB_PKG_PATH}/service $p"
-  echo "$PB_GENERATE_CMD"
-  eval "${PB_GENERATE_CMD}"
-  echo "$PB_PROXY_GENERATE_CMD"
-  eval "${PB_PROXY_GENERATE_CMD}"
-done
+echo "SUCCESS"
